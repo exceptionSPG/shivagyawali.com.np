94fb81ada33b556e6796fbe635a69da9e085aa5c
gyawalishiva14 <gyawalishiva14@gmail.com>
2026-06-22 20:36:47 +0000

    feat(menu): data-driven menu with external link & button support
    - src/util/menu.json drives the nav: Home, About, Blog, Category
      (/categories/), Homelab (/homelab/), YouTube (external button)
    - Contact removed from the menu (page itself kept)
    - navigation.js renders internal links, external links (target=_blank),
      and an optional button style from the config
    
    NOTE: replace the placeholder YouTube URL in src/util/menu.json.

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,31 +4,49 @@ import React from "react"
 import { Link } from "gatsby"
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri"
 import Theme from "../components/theme"
+import menuItems from "../util/menu.json"
 
-const MenuItems = [
-  {
-    path: "/",
-    title: "Home",
-  },
-  {
-    path: "/about",
-    title: "About",
-  },
-  {
-    path: "/blog",
-    title: "Blog",
-  },
-  {
-    path: "/contact",
-    title: "Contact",
-  },
-]
+// Menu is data-driven from src/util/menu.json.
+// Each item supports:
+//   { "title": "Blog", "path": "/blog" }                      internal link
+//   { "title": "YT", "url": "https://…", "external": true }   external link
+//   add "button": true to render it as a highlighted button
+const buttonStyle = {
+  display: "inline-block",
+  bg: "#FF0000",
+  color: "#fff",
+  borderRadius: "4px",
+  px: 3,
+  py: 1,
+  "&:hover": { color: "#fff", opacity: 0.9 },
+}
 
-const ListLink = props => (
-  <li>
-    <Link to={props.to}>{props.children}</Link>
-  </li>
-)
+const MenuLink = ({ item }) => {
+  const sxProp = item.button ? buttonStyle : {}
+
+  if (item.external) {
+    return (
+      <li>
+        <a
+          href={item.url}
+          target="_blank"
+          rel="noopener noreferrer"
+          sx={sxProp}
+        >
+          {item.title}
+        </a>
+      </li>
+    )
+  }
+
+  return (
+    <li>
+      <Link to={item.path} sx={sxProp}>
+        {item.title}
+      </Link>
+    </li>
+  )
+}
 
 class Navigation extends React.Component {
   constructor(props) {
@@ -44,10 +62,8 @@ class Navigation extends React.Component {
   }
 
   render() {
-    const listMenuItems = MenuItems.map((menuItem, index) => (
-      <ListLink key={index} to={menuItem.path}>
-        {menuItem.title}
-      </ListLink>
+    const listMenuItems = menuItems.map((item, index) => (
+      <MenuLink key={index} item={item} />
     ))
     return (
       <nav className="site-navigation" sx={navStyle.menu}>
